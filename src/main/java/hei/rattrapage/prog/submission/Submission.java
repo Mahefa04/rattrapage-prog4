1b023791cb360d62d3db8573f9444a32a5b20f4c
Mahefa04 <hei.mahefa.2@gmail.com>
2026-09-16 08:41:59 +0300

    feat: add dependencies

---
+++ src/main/java/hei/rattrapage/prog/submission/Submission.java
@@ -0,0 +1,31 @@
+package hei.rattrapage.prog.submission;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Submission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String thumbnailKey;
+
+    public Submission(String email) {
+        this.email = email;
+        this.thumbnailKey = null;
+    }
+
+    public void updateThumbnailKey(String thumbnailKey) {
+        this.thumbnailKey = thumbnailKey;
+    }
+}
