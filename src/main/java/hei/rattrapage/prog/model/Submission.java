14c0e0b0eb10c22b1fb8b8175d226498830b8e90
Mahefa04 <hei.mahefa.2@gmail.com>
2026-09-16 08:53:09 +0300

    fix: modification in submission.java

--- src/main/java/hei/rattrapage/prog/model/Submission.java
+++ src/main/java/hei/rattrapage/prog/model/Submission.java
@@ -1,28 +1,33 @@
 package hei.rattrapage.prog.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "submissions")
 @Getter
 @NoArgsConstructor
 public class Submission {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
     private String email;
 
     private String thumbnailKey;
 
+    private Instant createdAt;
+
     public Submission(String email) {
+        this.id = UUID.randomUUID();
         this.email = email;
         this.thumbnailKey = null;
+        this.createdAt = Instant.now();
     }
 
     public void updateThumbnailKey(String thumbnailKey) {
